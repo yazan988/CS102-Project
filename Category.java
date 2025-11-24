59f58ff430fe251dc7c9633950003b47f620a385
yzaan albalawi <yazanalbalawi8@gmail.com>
2025-11-24 18:28:23 +0300

    Implement Category class with full encapsulation

---
+++ Category.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package InventoryManager;
+
+/**
+ *
+ * @author yazan
+ */
+//  Create Category class with encapsulation
+public class Category {
+    private String name;
+    private String description;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+
+    @Override
+    public String toString() {
+        return name + " (" + description + ")";
+    }
+}
